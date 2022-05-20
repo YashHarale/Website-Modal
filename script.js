de19934eb4600297558a751c1554b655da4bcb72
Yash <yashharale2003@gmail.com>
2022-05-20 23:19:32 +0530

    code review

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-const openModal = function () {
+const openModal = () => {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
